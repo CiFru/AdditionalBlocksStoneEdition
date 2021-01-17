e08bc7f099623746afedf039a58058bd23a7cf25
CiFru <rutgersucks@gmail.com>
2021-01-17 19:16:02 +0000

    added blocks

--- src/main/java/com/supermartijn642/additionalblocks/AdditionalBlocks.java
+++ src/main/java/com/supermartijn642/additionalblocks/AdditionalBlocks.java
@@ -26,103 +26,75 @@ public class AdditionalBlocks {
     private static final List<Block> blocks = new ArrayList<>();
     private static final List<Item> items = new ArrayList<>();
 
-    @ObjectHolder("additionalblocks:marble")
-    public static Block marble;
-    @ObjectHolder("additionalblocks:smooth_marble")
-    public static Block smooth_marble;
-    @ObjectHolder("additionalblocks:stone_brick_block")
-    public static Block stone_brick_block;
-    @ObjectHolder("additionalblocks:marble_bricks")
-    public static Block marble_bricks;
-    @ObjectHolder("additionalblocks:diorite_bricks")
-    public static Block diorite_bricks;
-    @ObjectHolder("additionalblocks:andesite_bricks")
-    public static Block andesite_bricks;
-    @ObjectHolder("additionalblocks:granite_bricks")
-    public static Block granite_bricks;
-    @ObjectHolder("additionalblocks:animated_block")
-    public static Block animated_block;
-    @ObjectHolder("additionalblocks:marble_pillar")
-    public static Block marble_pillar;
-    @ObjectHolder("additionalblocks:bloodstone")
-    public static Block bloodstone;
-    @ObjectHolder("additionalblocks:smooth_bloodstone")
-    public static Block smooth_bloodstone;
-    @ObjectHolder("additionalblocks:bloodstone_bricks")
-    public static Block bloodstone_bricks;
-    @ObjectHolder("additionalblocks:black_marble")
-    public static Block black_marble;
-    @ObjectHolder("additionalblocks:smooth_black_marble")
-    public static Block smooth_black_marble;
-    @ObjectHolder("additionalblocks:black_marble_bricks")
-    public static Block black_marble_bricks;
-    @ObjectHolder("additionalblocks:mud")
-    public static Block mud;
-    @ObjectHolder("additionalblocks:brown_bricks")
-    public static Block brown_bricks;
 
-    public AdditionalBlocks(){
+    public static Block marble;
+    public static Block smooth_marble;
+    public static Block stone_brick_block;
+    public static Block marble_bricks;
+    public static Block diorite_bricks;
+    public static Block andesite_bricks;
+    public static Block granite_bricks;
+    public static Block animated_block;
+    public static Block marble_pillar;
+    public static Block bloodstone;
+    public static Block smooth_bloodstone;
+    public static Block bloodstone_bricks;
+    public static Block black_marble;
+    public static Block smooth_black_marble;
+    public static Block black_marble_bricks;
+    public static Block mud;
+    public static Block brown_bricks;
+    public static Block volcanic_stone;
+    public static Block volcanic_stone_bricks;
+
+    public AdditionalBlocks() {
     }
 
     @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
         @SubscribeEvent
-        public static void onBlockRegistry(final RegistryEvent.Register<Block> e){
-            e.getRegistry().register(new BasicBlock("marble", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.PINK).sound(SoundType.STONE)));
-            e.getRegistry().register(new BasicBlock("smooth_marble", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.PINK).sound(SoundType.STONE)));
-            e.getRegistry().register(new BasicBlock("stone_brick_block", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.GRAY).sound(SoundType.STONE)));
-            e.getRegistry().register(new BasicBlock("marble_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.PINK).sound(SoundType.STONE)));
-            e.getRegistry().register(new BasicBlock("diorite_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.LIGHT_GRAY).sound(SoundType.STONE)));
-            e.getRegistry().register(new BasicBlock("andesite_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.GRAY).sound(SoundType.STONE)));
-            e.getRegistry().register(new BasicBlock("granite_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BROWN).sound(SoundType.STONE)));
-            e.getRegistry().register(new BasicBlock("animated_block", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.GRAY).sound(SoundType.STONE)));
-            e.getRegistry().register(new AxisRotationBlock("marble_pillar", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.PINK).sound(SoundType.STONE)));
-            e.getRegistry().register(new AxisRotationBlock("bloodstone", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BLACK).sound(SoundType.STONE)));
-            e.getRegistry().register(new AxisRotationBlock("smooth_bloodstone", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BLACK).sound(SoundType.STONE)));
-            e.getRegistry().register(new AxisRotationBlock("bloodstone_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BLACK).sound(SoundType.STONE)));
-            e.getRegistry().register(new AxisRotationBlock("black_marble", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BLACK).sound(SoundType.STONE)));
-            e.getRegistry().register(new AxisRotationBlock("smooth_black_marble", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BLACK).sound(SoundType.STONE)));
-            e.getRegistry().register(new AxisRotationBlock("black_marble_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BLACK).sound(SoundType.STONE)));
-            e.getRegistry().register(new AxisRotationBlock("mud", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BROWN).sound(SoundType.WET_GRASS)));
-            e.getRegistry().register(new AxisRotationBlock("brown_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BROWN).sound(SoundType.NETHER_BRICK)));
+        public static void onBlockRegistry(final RegistryEvent.Register<Block> e) {
+            marble = registerBlock(e, new BasicBlock("marble", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.PINK).sound(SoundType.STONE)));
+            smooth_marble = registerBlock(e, new BasicBlock("smooth_marble", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.PINK).sound(SoundType.STONE)));
+            stone_brick_block = registerBlock(e, new BasicBlock("stone_brick_block", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.GRAY).sound(SoundType.STONE)));
+            marble_bricks = registerBlock(e, new BasicBlock("marble_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.PINK).sound(SoundType.STONE)));
+            diorite_bricks = registerBlock(e, new BasicBlock("diorite_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.LIGHT_GRAY).sound(SoundType.STONE)));
+            andesite_bricks = registerBlock(e, new BasicBlock("andesite_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.GRAY).sound(SoundType.STONE)));
+            granite_bricks = registerBlock(e, new BasicBlock("granite_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BROWN).sound(SoundType.STONE)));
+            animated_block = registerBlock(e, new BasicBlock("animated_block", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.GRAY).sound(SoundType.STONE)));
+            marble_pillar = registerBlock(e, new AxisRotationBlock("marble_pillar", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.PINK).sound(SoundType.STONE)));
+            bloodstone = registerBlock(e, new BasicBlock("bloodstone", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BLACK).sound(SoundType.STONE)));
+            smooth_bloodstone = registerBlock(e, new BasicBlock("smooth_bloodstone", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BLACK).sound(SoundType.STONE)));
+            bloodstone_bricks = registerBlock(e, new BasicBlock("bloodstone_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BLACK).sound(SoundType.STONE)));
+            black_marble = registerBlock(e, new BasicBlock("black_marble", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BLACK).sound(SoundType.STONE)));
+            smooth_black_marble = registerBlock(e, new BasicBlock("smooth_black_marble", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BLACK).sound(SoundType.STONE)));
+            black_marble_bricks = registerBlock(e, new BasicBlock("black_marble_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BLACK).sound(SoundType.STONE)));
+            mud = registerBlock(e, new BasicBlock("mud", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BROWN).sound(SoundType.WET_GRASS)));
+            brown_bricks = registerBlock(e, new BasicBlock("brown_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.BROWN).sound(SoundType.NETHER_BRICK)));
+            volcanic_stone = registerBlock(e, new BasicBlock("volcanic_stone", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.GRAY).sound(SoundType.NETHER_BRICK)));
+            volcanic_stone_bricks = registerBlock(e, new BasicBlock("volcanic_stone_bricks", AbstractBlock.Properties.create(Material.ROCK, MaterialColor.GRAY).sound(SoundType.NETHER_BRICK)));
+            registerBlock(e, new StairBlock(marble,AbstractBlock.Properties.create(Material.ROCK, MaterialColor.PINK).sound(SoundType.STONE)));
+            registerBlock(e, new SlabBlock(marble,AbstractBlock.Properties.create(Material.ROCK, MaterialColor.PINK).sound(SoundType.STONE)));
         }
 
         @SubscribeEvent
-        public static void onTileRegistry(final RegistryEvent.Register<TileEntityType<?>> e){
+        public static void onTileRegistry(final RegistryEvent.Register<TileEntityType<?>> e) {
         }
 
         @SubscribeEvent
-        public static void onItemRegistry(final RegistryEvent.Register<Item> e){
-            for(Block block : blocks)
+        public static void onItemRegistry(final RegistryEvent.Register<Item> e) {
+            for (Block block : blocks)
                 registerItem(e, new BlockItem(block, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(block.getRegistryName()));
-
-            e.getRegistry().register(new BlockItem(marble, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(marble.getRegistryName()));
-            e.getRegistry().register(new BlockItem(smooth_marble, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(smooth_marble.getRegistryName()));
-            e.getRegistry().register(new BlockItem(stone_brick_block, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(stone_brick_block.getRegistryName()));
-            e.getRegistry().register(new BlockItem(marble_bricks, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(marble_bricks.getRegistryName()));
-            e.getRegistry().register(new BlockItem(diorite_bricks, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(diorite_bricks.getRegistryName()));
-            e.getRegistry().register(new BlockItem(andesite_bricks, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(andesite_bricks.getRegistryName()));
-            e.getRegistry().register(new BlockItem(granite_bricks, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(granite_bricks.getRegistryName()));
-            e.getRegistry().register(new BlockItem(animated_block, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(animated_block.getRegistryName()));
-            e.getRegistry().register(new BlockItem(marble_pillar, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(marble_pillar.getRegistryName()));
-            e.getRegistry().register(new BlockItem(bloodstone, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(bloodstone.getRegistryName()));
-            e.getRegistry().register(new BlockItem(smooth_bloodstone, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(smooth_bloodstone.getRegistryName()));
-            e.getRegistry().register(new BlockItem(bloodstone_bricks, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(bloodstone_bricks.getRegistryName()));
-            e.getRegistry().register(new BlockItem(black_marble, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(black_marble.getRegistryName()));
-            e.getRegistry().register(new BlockItem(smooth_black_marble, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(smooth_black_marble.getRegistryName()));
-            e.getRegistry().register(new BlockItem(black_marble_bricks, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(black_marble_bricks.getRegistryName()));
-            e.getRegistry().register(new BlockItem(mud, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(mud.getRegistryName()));
-            e.getRegistry().register(new BlockItem(brown_bricks, new Item.Properties().group(ItemGroup.SEARCH)).setRegistryName(brown_bricks.getRegistryName()));
         }
     }
 
-    public static <T extends Block> T registerBlock(RegistryEvent.Register<Block> e, T block){
+    public static <T extends Block> T registerBlock(RegistryEvent.Register<Block> e, T block) {
         e.getRegistry().register(block);
         blocks.add(block);
         return block;
     }
 
-    public static <T extends Item> T registerItem(RegistryEvent.Register<Item> e, T item){
+    public static <T extends Item> T registerItem(RegistryEvent.Register<Item> e, T item) {
         e.getRegistry().register(item);
         items.add(item);
         return item;
