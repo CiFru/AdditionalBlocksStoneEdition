74a9e8f97a4b2879a789b8b29d84ff03f8498256
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-01-20 23:36:21 +0100

    disable WorldGeneration because of 1.16.1

--- src/main/java/com/supermartijn642/additionalblocks/stone/WorldGeneration.java
+++ src/main/java/com/supermartijn642/additionalblocks/stone/WorldGeneration.java
@@ -22,35 +22,35 @@ import net.minecraftforge.registries.ForgeRegistries;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class WorldGeneration {
 
-    public static ConfiguredFeature<?,?> ore_marble;
+//    public static ConfiguredFeature<?,?> ore_marble;
 
-    public static void onFeatureRegistry(final RegistryEvent.Register<Feature<?>> e){
-        ore_marble = Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, AdditionalBlocks.smooth_marble.getDefaultState(), 33));
-        ore_marble.range(80).square().func_242731_b(10);
-        Registry.register(WorldGenRegistries.CONFIGURED_FEATURE,
-            new ResourceLocation("abstoneedition", "ore_marble"),
-            ore_marble
-        );
-    }
+//    public static void onFeatureRegistry(final RegistryEvent.Register<Feature<?>> e){
+//        ore_marble = Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, AdditionalBlocks.smooth_marble.getDefaultState(), 33));
+//        ore_marble.range(80).square().func_242731_b(10);
+//        Registry.register(WorldGenRegistries.CONFIGURED_FEATURE,
+//            new ResourceLocation("abstoneedition", "ore_marble"),
+//            ore_marble
+//        );
+//    }
 
-    @SubscribeEvent(priority = EventPriority.LOW)
-    public static void onBiomeLoad(BiomeLoadingEvent e){
-        RegistryKey<Biome> biomeKey = RegistryKey.getOrCreateKey(ForgeRegistries.Keys.BIOMES, e.getName());
-
-        // all overworld biomes
-        if(BiomeDictionary.getBiomes(BiomeDictionary.Type.OVERWORLD).contains(biomeKey)){
-
-        }
-
-        // all nether biomes
-        if(BiomeDictionary.getBiomes(BiomeDictionary.Type.OVERWORLD).contains(biomeKey)){
-
-        }
-
-        // all end biomes
-        if(BiomeDictionary.getBiomes(BiomeDictionary.Type.OVERWORLD).contains(biomeKey)){
-
-        }
-    }
+//    @SubscribeEvent(priority = EventPriority.LOW)
+//    public static void onBiomeLoad(BiomeLoadingEvent e){
+//        RegistryKey<Biome> biomeKey = RegistryKey.getOrCreateKey(ForgeRegistries.Keys.BIOMES, e.getName());
+//
+//        // all overworld biomes
+//        if(BiomeDictionary.getBiomes(BiomeDictionary.Type.OVERWORLD).contains(biomeKey)){
+//
+//        }
+//
+//        // all nether biomes
+//        if(BiomeDictionary.getBiomes(BiomeDictionary.Type.OVERWORLD).contains(biomeKey)){
+//
+//        }
+//
+//        // all end biomes
+//        if(BiomeDictionary.getBiomes(BiomeDictionary.Type.OVERWORLD).contains(biomeKey)){
+//
+//        }
+//    }
 
 }
