586dec9405c7546f10030ff114a471f97d60956e
CiFru <rutgersucks@gmail.com>
2021-07-24 01:09:15 +0200

    new walls registered, more slabs and stairs

--- src/main/java/com/supermartijn642/additionalblocks/stone/StairBlock.java
+++ src/main/java/com/supermartijn642/additionalblocks/stone/StairBlock.java
@@ -6,23 +6,32 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 
+import java.util.function.Supplier;
+
 /**
  * Created 1/17/2021 by SuperMartijn642
  */
 public class StairBlock extends StairsBlock implements IConfigObject, IItemGroupIndex {
 
-    private final BasicBlock block;
+    private final Block block;
+    private final Supplier<Boolean> enabled;
 
     public StairBlock(BasicBlock block, Properties properties) {
+        this(block, block.getRegistryName().getPath() + "_stairs", properties);
+    }
+
+    public StairBlock(Block block, Supplier<Boolean> enabled, Properties properties) {
         super(block::getDefaultState, properties);
         this.setRegistryName(block.getRegistryName().getPath() + "_stairs");
         this.block = block;
+        this.enabled = enabled;
     }
 
     public StairBlock(BasicBlock block, String registryName, Properties properties) {
         super(block::getDefaultState, properties);
         this.setRegistryName(registryName);
         this.block = block;
+        this.enabled = block::isEnabled;
     }
 
     @Override
@@ -33,7 +42,7 @@ public class StairBlock extends StairsBlock implements IConfigObject, IItemGroup
 
     @Override
     public boolean isEnabled() {
-        return this.block.isEnabled();
+        return this.enabled.get();
     }
 
     @Override
--- src/main/java/com/supermartijn642/additionalblocks/stone/WallyBlock.java
+++ src/main/java/com/supermartijn642/additionalblocks/stone/WallyBlock.java
@@ -1,32 +1,30 @@
 package com.supermartijn642.additionalblocks.stone;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.WallBlock;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 
 import java.util.function.Supplier;
 
-/**
- * Created 1/17/2021 by SuperMartijn642
- */
-public class SlabBlock extends net.minecraft.block.SlabBlock implements IConfigObject, IItemGroupIndex {
+public class WallyBlock extends WallBlock implements IConfigObject, IItemGroupIndex {
 
     private final Block block;
     private final Supplier<Boolean> enabled;
 
-    public SlabBlock(BasicBlock block, Properties properties) {
-        this(block, block.getRegistryName().getPath() + "_slab", properties);
+    public WallyBlock(BasicBlock block, Properties properties) {
+        this(block, block.getRegistryName().getPath() + "_wall", properties);
     }
 
-    public SlabBlock(Block block, Supplier<Boolean> enabled, Properties properties) {
+    public WallyBlock(Block block, Supplier<Boolean> enabled, Properties properties) {
         super(properties);
-        this.setRegistryName(block.getRegistryName().getPath() + "_slab");
+        this.setRegistryName(block.getRegistryName().getPath() + "_wall");
         this.block = block;
         this.enabled = enabled;
     }
 
-    public SlabBlock(BasicBlock block, String registryName, Properties properties) {
+    public WallyBlock(BasicBlock block, String registryName, Properties properties) {
         super(properties);
         this.setRegistryName(registryName);
         this.block = block;
@@ -46,6 +44,6 @@ public class SlabBlock extends net.minecraft.block.SlabBlock implements IConfigO
 
     @Override
     public int getItemGroupIndex() {
-        return 2;
+        return 3;
     }
 }
