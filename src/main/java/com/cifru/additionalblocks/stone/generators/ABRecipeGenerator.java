17e80e25ede9dc16c809f6dceba6f0ebd2b7d968
CiFru <rutgersucks@gmail.com>
2023-06-23 14:21:29 +0200

    Fixed crash and wrong recipes

--- src/main/java/com/cifru/additionalblocks/stone/generators/ABRecipeGenerator.java
+++ src/main/java/com/cifru/additionalblocks/stone/generators/ABRecipeGenerator.java
@@ -606,8 +606,8 @@ public class ABRecipeGenerator extends RecipeGenerator {
             .condition(new ItemEnabledResourceCondition(AdditionalBlocksItems.COPPER_HOE));
         // Copper sword
         this.shaped(AdditionalBlocksItems.COPPER_SWORD)
-            .pattern("AAA")
-            .pattern(" B ")
+            .pattern(" A ")
+            .pattern(" A ")
             .pattern(" B ")
             .input('A', copperIngots)
             .input('B', Tags.Items.RODS_WOODEN)
@@ -710,8 +710,8 @@ public class ABRecipeGenerator extends RecipeGenerator {
             .condition(new ItemEnabledResourceCondition(AdditionalBlocksItems.SILVER_HOE));
         // Silver sword
         this.shaped(AdditionalBlocksItems.SILVER_SWORD)
-            .pattern("AAA")
-            .pattern(" B ")
+            .pattern(" A ")
+            .pattern(" A ")
             .pattern(" B ")
             .input('A', silverIngots)
             .input('B', Tags.Items.RODS_WOODEN)
